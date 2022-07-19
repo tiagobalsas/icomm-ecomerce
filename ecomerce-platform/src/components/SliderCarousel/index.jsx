6b2f0b9398390d2def1a1be04c04f4eff3daab3b
Tiago Santos <tiago881@gmail.com>
2022-07-19 09:42:07 -0300

    feat:finished carousel

--- ecomerce-platform/src/components/SliderCarousel/index.jsx
+++ ecomerce-platform/src/components/SliderCarousel/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Slider from 'react-slick';
+import { RiArrowLeftSLine, RiArrowRightSLine } from 'react-icons/ri';
 
 import './style.css';
 
@@ -14,16 +15,66 @@ function SliderCarousel() {
     slidesToScroll: 1,
   };
 
+  const stylesPrevArrow = {
+    backgroundColor: "#EEEEEE",
+    border: "none",
+    position: "absolute",
+    left: "-2rem",
+    top: "8rem",
+    cursor: "pointer",
+  };
+
+  const stylesNextArrow = {
+    backgroundColor: "#EEEEEE",
+    border: "none",
+    position: "absolute",
+    right: "-2rem",
+    top: "8rem",
+    cursor: "pointer",
+  };
+
+  const PrevArrow = (props) => {
+    const { onClick, style } = props;
+
+    return (
+      <button
+        type='button'
+        onClick={onClick}
+        style={{ ...style, ...stylesPrevArrow }}
+      >
+        <RiArrowLeftSLine />
+      </button>
+    );
+  };
+
+  const NextArrow = (props) => {
+    const { onClick, style } = props;
+
+    return (
+      <button
+        type='button'
+        onClick={onClick}
+        style={{ ...style, ...stylesNextArrow }}
+      >
+        <RiArrowRightSLine />
+      </button>
+    );
+  };
+
   return (
     <section className='products'>
       <div className='products-container'>
-        <div className='products__title'>
+        <div className='products-title'>
           <h1>DESTAQUES</h1>
         </div>
-        <div className='products__carousel'>
-          <Slider {...settings}>
-            {data.map(({ id, image, product, price, price_x }) => (
-              <div key={id}>
+        <div className='products-carousel'>
+          <Slider
+            prevArrow={<PrevArrow />}
+            nextArrow={<NextArrow />}
+            {...settings}
+          >
+            {data.map(({ image, product, price, price_x }) => (
+              <div>
                 <div className='product-container'>
                   <img className='product-img' src={image} alt={product} />
                   <div className='product-info'>
